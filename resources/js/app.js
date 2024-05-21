275612a5203dd411c272a0e743e8a80dafabe917
Jay Shyam Patel <jayshyampatel133@gmail.com>
2024-05-22 02:03:50 +0545

    chore: Update Vue components and add WebSocket functionality for chat and video calls

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,29 @@
-import { createApp } from 'vue'
-import Counter from './components/Counter.vue'
+require('./bootstrap');
 
-const app = createApp()
+window.Vue = require('vue');
 
-app.component('counter', Counter)
+import VueChatScroll from 'vue-chat-scroll'
+Vue.use(require('vue-chat-scroll'))
 
-app.mount('#app')
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('main-component', require('./components/MainComponent.vue').default);
+Vue.component('user-list-component', require('./components/UserListComponent.vue').default);
+Vue.component('chat-component', require('./components/ChatComponent.vue').default);
+Vue.component('video-component', require('./components/VideoComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    created(){
+        Echo.channel('updateUserStatus')
+        .listen('UpdateUserOnlineStatus',(e) =>{
+            console.log(e);
+        });
+    }
+});
